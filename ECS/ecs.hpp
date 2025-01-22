01e5942b033599adf823cbedfc5f81818f50ece3
Ковалев Владислав <vlad.kovalev1503@gmail.com>
2025-01-22 08:30:07 +0300

    Поменял названия файлов и папок

--- ECS/ecs.hpp
+++ ECS/ecs.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "./src/flecs alike/system_manager.hpp"
-#include "./src/flecs alike/entity_manager.hpp"
-#include "./src/flecs alike/context.hpp"
-#include "./src/flecs alike/looper.hpp"
+#include "./src/flecs_alike/system_manager.hpp"
+#include "./src/flecs_alike/entity_manager.hpp"
+#include "./src/flecs_alike/context.hpp"
+#include "./src/flecs_alike/looper.hpp"
